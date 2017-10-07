86ade930d436ba436122aa3475c0a9cc3441c0e8
Alvin Chan <aschan@gmail.com>
2017-10-07 12:27:12 -0700

    Automatically format code on `precommit`

--- src/index.js
+++ src/index.js
@@ -1,201 +1,216 @@
 // Groups of CSS properties in Concentric order
 const groups = [
-  ["box-sizing"],
-  ["display", "position", "top", "right", "bottom", "left"],
-  ["float", "clear"],
-  ["align-content", "align-items", "align-self"],
+  ['box-sizing'],
+  ['display', 'position', 'top', 'right', 'bottom', 'left'],
+  ['float', 'clear'],
+  ['align-content', 'align-items', 'align-self'],
   [
-    "flex",
-    "flex-basis",
-    "flex-direction",
-    "flex-flow",
-    "flex-grow",
-    "flex-shrink",
-    "flex-wrap",
-    "justify-content"
+    'flex',
+    'flex-basis',
+    'flex-direction',
+    'flex-flow',
+    'flex-grow',
+    'flex-shrink',
+    'flex-wrap',
+    'justify-content'
   ],
-  ["order"],
+  ['order'],
   [
-    "columns",
-    "column-gap",
-    "column-fill",
-    "column-rule",
-    "column-rule-width",
-    "column-rule-style",
-    "column-rule-color",
-    "column-span",
-    "column-count",
-    "column-width"
-  ],
-  [
-    "backface-visibility",
-    "perspective",
-    "perspective-origin",
-    "transform",
-    "transform-origin",
-    "transform-style"
+    'columns',
+    'column-gap',
+    'column-fill',
+    'column-rule',
+    'column-rule-width',
+    'column-rule-style',
+    'column-rule-color',
+    'column-span',
+    'column-count',
+    'column-width'
   ],
   [
-    "transition",
-    "transition-delay",
-    "transition-duration",
-    "transition-property",
-    "transition-timing-function"
-  ],
-  ["visibility", "opacity", "z-index"],
-  ["margin", "margin-top", "margin-right", "margin-bottom", "margin-left"],
-  [
-    "outline",
-    "outline-offset",
-    "outline-width",
-    "outline-style",
-    "outline-color"
+    'backface-visibility',
+    'perspective',
+    'perspective-origin',
+    'transform',
+    'transform-origin',
+    'transform-style'
   ],
   [
-    "border",
-    "border-top",
-    "border-right",
-    "border-bottom",
-    "border-left",
-    "border-width",
-    "border-top-width",
-    "border-right-width",
-    "border-bottom-width",
-    "border-left-width"
+    'transition',
+    'transition-delay',
+    'transition-duration',
+    'transition-property',
+    'transition-timing-function'
+  ],
+  ['visibility', 'opacity', 'z-index'],
+  ['margin', 'margin-top', 'margin-right', 'margin-bottom', 'margin-left'],
+  [
+    'outline',
+    'outline-offset',
+    'outline-width',
+    'outline-style',
+    'outline-color'
   ],
   [
-    "border-style",
-    "border-top-style",
-    "border-right-style",
-    "border-bottom-style",
-    "border-left-style"
+    'border',
+    'border-top',
+    'border-right',
+    'border-bottom',
+    'border-left',
+    'border-width',
+    'border-top-width',
+    'border-right-width',
+    'border-bottom-width',
+    'border-left-width'
   ],
   [
-    "border-radius",
-    "border-top-left-radius",
-    "border-top-right-radius",
-    "border-bottom-left-radius",
-    "border-bottom-right-radius"
+    'border-style',
+    'border-top-style',
+    'border-right-style',
+    'border-bottom-style',
+    'border-left-style'
   ],
   [
-    "border-color",
-    "border-top-color",
-    "border-right-color",
-    "border-bottom-color",
-    "border-left-color"
+    'border-radius',
+    'border-top-left-radius',
+    'border-top-right-radius',
+    'border-bottom-left-radius',
+    'border-bottom-right-radius'
   ],
   [
-    "border-image",
-    "border-image-source",
-    "border-image-width",
-    "border-image-outset",
-    "border-image-repeat",
-    "border-image-slice"
-  ],
-  ["box-shadow"],
-  [
-    "background",
-    "background-attachment",
-    "background-clip",
-    "background-color",
-    "background-image",
-    "background-origin",
-    "background-position",
-    "background-repeat",
-    "background-size"
-  ],
-  ["cursor"],
-  ["padding", "padding-top", "padding-right", "padding-bottom", "padding-left"],
-  ["width", "min-width", "max-width"],
-  ["height", "min-height", "max-height"],
-  ["overflow", "overflow-x", "overflow-y", "resize"],
-  [
-    "list-style",
-    "list-style-type",
-    "list-style-position",
-    "list-style-image",
-    "caption-side"
-  ],
-  ["table-layout", "border-collapse", "border-spacing", "empty-cells"],
-  [
-    "animation",
-    "animation-name",
-    "animation-duration",
-    "animation-timing-function",
-    "animation-delay",
-    "animation-iteration-count",
-    "animation-direction",
-    "animation-fill-mode",
-    "animation-play-state"
-  ],
-  ["vertical-align"],
-  [
-    "direction",
-    "tab-size",
-    "text-align",
-    "text-align-last",
-    "text-justify",
-    "text-indent",
-    "text-transform",
-    "text-decoration",
-    "text-decoration-color",
-    "text-decoration-line",
-    "text-decoration-style",
-    "text-rendering",
-    "text-shadow",
-    "text-overflow"
+    'border-color',
+    'border-top-color',
+    'border-right-color',
+    'border-bottom-color',
+    'border-left-color'
   ],
   [
-    "line-height",
-    "word-spacing",
-    "letter-spacing",
-    "white-space",
-    "word-break",
-    "word-wrap",
-    "color"
+    'border-image',
+    'border-image-source',
+    'border-image-width',
+    'border-image-outset',
+    'border-image-repeat',
+    'border-image-slice'
+  ],
+  ['box-shadow'],
+  [
+    'background',
+    'background-attachment',
+    'background-clip',
+    'background-color',
+    'background-image',
+    'background-origin',
+    'background-position',
+    'background-repeat',
+    'background-size'
+  ],
+  ['cursor'],
+  ['padding', 'padding-top', 'padding-right', 'padding-bottom', 'padding-left'],
+  ['width', 'min-width', 'max-width'],
+  ['height', 'min-height', 'max-height'],
+  ['overflow', 'overflow-x', 'overflow-y', 'resize'],
+  [
+    'list-style',
+    'list-style-type',
+    'list-style-position',
+    'list-style-image',
+    'caption-side'
+  ],
+  ['table-layout', 'border-collapse', 'border-spacing', 'empty-cells'],
+  [
+    'animation',
+    'animation-name',
+    'animation-duration',
+    'animation-timing-function',
+    'animation-delay',
+    'animation-iteration-count',
+    'animation-direction',
+    'animation-fill-mode',
+    'animation-play-state'
+  ],
+  ['vertical-align'],
+  [
+    'direction',
+    'tab-size',
+    'text-align',
+    'text-align-last',
+    'text-justify',
+    'text-indent',
+    'text-transform',
+    'text-decoration',
+    'text-decoration-color',
+    'text-decoration-line',
+    'text-decoration-style',
+    'text-rendering',
+    'text-shadow',
+    'text-overflow'
   ],
   [
-    "font",
-    "font-family",
-    "font-size",
-    "font-size-adjust",
-    "font-stretch",
-    "font-weight",
-    "font-smoothing",
-    "osx-font-smoothing",
-    "font-variant",
-    "font-style"
+    'line-height',
+    'word-spacing',
+    'letter-spacing',
+    'white-space',
+    'word-break',
+    'word-wrap',
+    'color'
   ],
-  ["content", "quotes"],
-  ["counter-reset", "counter-increment"],
-  ["page-break-before", "page-break-after", "page-break-inside"]
-];
+  [
+    'font',
+    'font-family',
+    'font-size',
+    'font-size-adjust',
+    'font-stretch',
+    'font-weight',
+    'font-smoothing',
+    'osx-font-smoothing',
+    'font-variant',
+    'font-style'
+  ],
+  ['content', 'quotes'],
+  ['counter-reset', 'counter-increment'],
+  ['page-break-before', 'page-break-after', 'page-break-inside']
+]
 
 // Export the Stylelint configuration
 module.exports = {
-  plugins: ["stylelint-order"],
+  plugins: ['stylelint-order'],
   rules: {
-    "order/order": [
-      "dollar-variables",
-      "custom-properties",
-      { type: "at-rule", name: "extend" },
-      { type: "at-rule", name: "include" },
-      { type: "at-rule", name: "mixin" },
-      { type: "at-rule", name: "add-mixin" },
-      "declarations",
-      "rules",
-      { type: "at-rule", name: "include", parameter: "breakpoint", hasBlock: true },
-      { type: "at-rule", name: "mixin", parameter: "breakpoint", hasBlock: true },
-      { type: "at-rule", name: "add-mixin", parameter: "breakpoint", hasBlock: true },
-      { type: "at-rule", name: "media", hasBlock: true }
+    'order/order': [
+      'dollar-variables',
+      'custom-properties',
+      { type: 'at-rule', name: 'extend' },
+      { type: 'at-rule', name: 'include' },
+      { type: 'at-rule', name: 'mixin' },
+      { type: 'at-rule', name: 'add-mixin' },
+      'declarations',
+      'rules',
+      {
+        type: 'at-rule',
+        name: 'include',
+        parameter: 'breakpoint',
+        hasBlock: true
+      },
+      {
+        type: 'at-rule',
+        name: 'mixin',
+        parameter: 'breakpoint',
+        hasBlock: true
+      },
+      {
+        type: 'at-rule',
+        name: 'add-mixin',
+        parameter: 'breakpoint',
+        hasBlock: true
+      },
+      { type: 'at-rule', name: 'media', hasBlock: true }
     ],
-    "order/properties-order": [
+    'order/properties-order': [
       groups.map(group => ({
         properties: group
       })),
       {
-        unspecified: "bottomAlphabetical"
+        unspecified: 'bottomAlphabetical'
       }
     ]
   }
-};
+}
